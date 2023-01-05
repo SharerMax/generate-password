2fd1704d542d1befd37111809dad5f9e8c5fe88e
Max Zhao <mdcw1103@gmail.com>
2023-01-05 14:06:14 +0800

    chore: update deps

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import PasswordGenerate from './pages/pwd-gen'
 import 'uno.css'
 import './index.css'
+import App from './App'
+
+const router = createBrowserRouter([{
+  path: '/',
+  element: <App/>,
+},
+{
+  path: '/password-generate',
+  element: <PasswordGenerate/>,
+},
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
-
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import unocss from 'unocss/vite'
+import Icons from 'unplugin-icons/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), unocss()],
+  plugins: [react(), unocss(), Icons({
+    compiler: 'jsx',
+    jsx: 'react',
+  })],
 })
